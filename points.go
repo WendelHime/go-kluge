83a140bce786613da416e3f8924db8fd433afd23
WendelHime <wendelhime@hotmail.com>
2021-02-27 13:10:35 -0300

    remove main and add cmd

--- points.go
+++ points.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"image"
-	"log"
 	"math"
 	"math/rand"
-	"os"
-	"time"
 
 	"github.com/fogleman/gg"
 )
@@ -76,36 +71,4 @@ func BuildLines(w, h int, points []*Point, minDist float64) *gg.Context {
 		}
 	}
 	return ctx
-}
-
-func main() {
-	// setting up vars
-	fp := flag.String("filepath", "/home/wotan/Pictures/ada_in_blank.png", "the filepath for a segmented image")
-	threshold := flag.Float64("threshold", 0.25, "a threshold for random creation of points, the value must be in the range 0, 100.")
-	minDist := flag.Float64("minDist", 70, "minimum distance of points")
-	output := flag.String("output", "./output.png", "output filepath")
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s: %s -filepath images/ada_in_blank.png -threshold 0.35 -minDist 50 -output images/ada_output.png\n", os.Args[0], os.Args[0])
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-	rand.Seed(time.Now().UnixNano())
-
-	// load image
-	img, err := gg.LoadImage(*fp)
-	if err != nil {
-		panic(err)
-	}
-	// get random points
-	points := GeneratePoints(img, *threshold)
-
-	// draw lines between points
-	ctx := BuildLines(img.Bounds().Max.X, img.Bounds().Max.Y, points, *minDist)
-
-	// save image
-	err = ctx.SavePNG(*output)
-	if err != nil {
-		log.Fatalf("fail saving png: %+v", err)
-	}
-
 }
